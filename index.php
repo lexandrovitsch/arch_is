a8af1bb1b68057c4af69bc97b9c63e7e84ce2691
lexaDOOM <rfvghm@gmail.com>
2025-03-11 16:39:10 +0300

    lab2 "dummy" added

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 	<body>
 		<p style="font-weight: bold; color: red;">Лабораторные работы</p>
 			<a href="lab1/index.php">Лабораторная работа №1</a>
+			<a href="lab2/index.php">Лабораторная работа №2</a>
 	</body>
 </html>
---
+++ lab2/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Лабораторная работа №2</title>
+	</head>
+	<body>
+		<p style="font-weight: bold; color: red;">Лабораторная работа №1</p>
+		<?php
+			echo 'Выполнил студент Прохоров А.А.<br>';
+			echo '------------------------------<br>';
+		?>
+	</body>
+</html>
